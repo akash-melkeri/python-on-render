f3867921f257db2da7d1e5381744c1b6088283b0
akash-melkeri <akashmelkeri23@gmail.com>
2024-04-15 21:33:22 +0530

    api route

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/')
+@app.route('/')
 def default():
     return jsonify(ok=True, message='Hello, World!')
+
+@app.route('/api/')
+def default():
+    return jsonify(ok=True, message='API, World!')
 
 if __name__ == '__main__':
     app.run(debug=True)
